b92c0e6e98639160dd9ddc395e4756d40b55c6c2
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2021-07-02 19:45:54 +0300

    Update pnoise.py

--- pnoise.py
+++ pnoise.py
@@ -1,20 +1,18 @@
 import random
 import decimal
 
-#height
-h = 10
-#base
-b = 5
+#size
+height = 10
+base = 5
 
-#axis that storage the all the axis_y
-axis_x = []
+axis_y = []
 
 #appending axis to the axis x based on size
-for x in range(h):
-	  axis_y = []
-	  for x in range(b):
-        axis_y.append(decimal.Decimal(random.randrange(0,101))/100)
-	  axis_x.append(axis_y)
+for y in range(height):
+	axis_x = []
+	for x in range(base):
+            axis_x.append(decimal.Decimal(random.randrange(0,101))/100)
+	axis_y.append(axis_x)
 
-for axis_y in axis_x:
-	  print(*axis_y)
+for axis_x in axis_y:
+    print(*axis_x)
